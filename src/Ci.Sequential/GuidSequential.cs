a5aaa9a4a243ae701d0c692e3305b4fe1dd3aaf3
Money Yu <abc12207@gmail.com>
2019-01-25 10:32:56 +0800

    add easy way to get sequential guid

---
+++ src/Ci.Sequential/GuidSequential.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ci.Sequential
+{
+    /// <summary>
+    /// Static methods to get an MSSQL-order variant COMB Guid for an easy way.
+    /// </summary>
+    public static class GuidSequential
+    {
+        /// <summary>
+        /// Initializes a new instance of the sequential <see cref="T:System.Guid" /> structure.
+        /// </summary>
+        /// <returns></returns>
+        public static System.Guid NewGuid() => Guid.Create(System.Guid.NewGuid(), DateTime.UtcNow);
+
+        /// <summary>
+        /// A read-only instance of the <see cref="T:System.Guid" /> structure whose value is all zeros.
+        /// </summary>
+        public static System.Guid Empty => System.Guid.Empty;
+    }
+}
